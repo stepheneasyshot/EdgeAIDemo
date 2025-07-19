2dda7737954de04f8efeb4afd79f032e4110988d
stephenzhan <zhanfeng990927@gmail.com>
2025-07-19 15:18:40 +0800

    initial commit

---
+++ composeApp/src/desktopMain/kotlin/com/stephen/smallmodel/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.stephen.smallmodel
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
---
+++ composeApp/src/iosMain/kotlin/com/stephen/smallmodel/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.stephen.smallmodel
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
